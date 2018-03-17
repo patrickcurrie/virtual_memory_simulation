83e8e46f537abe2de0f6609bf032983dc113b019
patrickcurrie <34009975+patrickcurrie@users.noreply.github.com>
2018-03-17 17:30:10 -0400

    Create source file for memory management logic

---
+++ memory_manager.c
@@ -0,0 +1,21 @@
+#include "memory_manager.h"
+
+static char physical_memory[8388608]; /* 8 MB to simulate physical memory */
+
+/*
+* Returns a void pointer to allocated memory.
+* Finds the page associated with the calling thread and allocated memory from it.
+* If there is not enough memory in the page to allocate from, return a NULL pointer.
+* Should communicate with scheduler to know which thread made the request so it knows which page to allocate from.
+*/
+void *my_allocate(int size, char *FILE, int *LINE, REQID threadreq) {
+        return NULL;
+}
+
+/*
+* Finds the page associated with the calling thread and frees allocated memory from it.
+* Should communicate with scheduler to know which thread made the request so it knows which page to free the allocation from.
+*/
+void my_deallocate(void *ptr, char *FILE, int *LINE, REQID threadreq) {
+
+}
