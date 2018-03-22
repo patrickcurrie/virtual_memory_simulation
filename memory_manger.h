e48dadf52a4ec024bddae651feb2d42bcc0080a7
Patrick Currie <34009975+patrickcurrie@users.noreply.github.com>
2018-03-21 21:20:55 -0400

    Keep track of total size of allocations in page

--- memory_manger.h
+++ memory_manger.h
@@ -1,19 +1,20 @@
 #include <stdlib.h>
 #include "my_pthread_t.h"
 
-enum REQID {
+enum REQUEST_ID {
         LIBRARYREQ,
         THREADREQ
 };
 
 struct block {
-        size_t size;
+        int size;
         char *data;
         struct block *next;
 }
 
 struct page {
         my_pthread_t tid;
+        int size_of_allocated;
         char *start_address;
         char *end_address;
         struct block *block_list_head;
@@ -25,10 +26,10 @@ struct page_list {
 }
 
 // When user calls malloc or free from a thread.
-#define malloc(x) my_allocate(x, __FILE__, __LINE__, THREADREQ)
+#define malloc(x) my_allocate(x, __FILE__, __LINE__, REQUEST_ID)
 
-#define free(x) my_deallocate(x, __FILE__, __LINE__, THREADREQ)
+#define free(x) my_deallocate(x, __FILE__, __LINE__, REQUEST_ID)
 
-void *my_allocate(int size, char *FILE, int *LINE, REQID threadreq);
+void *my_allocate(int size, char *FILE, int *LINE, REQUEST_ID request_id);
 
-void my_deallocate(void *ptr, char *FILE, int *LINE, REQID threadreq);
+void my_deallocate(void *ptr, char *FILE, int *LINE, REQUEST_ID request_id);
