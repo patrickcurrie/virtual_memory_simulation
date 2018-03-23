35fedbefec848b5860e15ba0d2599d16455077c6
SuvaShahria <1uknow2@gmail.com>
2018-03-23 05:24:18 -0400

    dealloc done

--- memory_manager.c
+++ memory_manager.c
@@ -10,29 +10,112 @@ int PAGE_SIZE;
 int NUMBER_PAGES;
 struct page_list PAGE_LIST;
 
-
 /*
-* Gets memory metadata.
+* Sets memory metadata and initial page metadata.
+* Everything in here needs to be associated with a block.
+* Two blocks need to be created, one to point to this metadata, the other to point to the rest.
 */
-static void init_memory_metadata(int size, char *FILE, int *LINE, REQID threadreq) {
+static void init_memory_metadata(REQUEST_ID request_id) {
         /* Set memory metadata. */
         PAGE_SIZE = (int) sysconf(_SC_PAGE_SIZE);
         NUMBER_PAGES = 8388608 / PAGE_SIZE;
-
         struct page pg;
-        if (threadreq = LIBRARYREQ)
+        if (request_id = LIBRARYREQ)
                 pg.tid = NULL;
         else
                 pg.tid = get_current_tid();
+        pg.size_of_allocated = 0; /* A block needs to be allocated for this metadata */
         pg.start_address = PHYSICAL_MEMORY;
         pg.end_address = PHYSICAL_MEMORY + (PAGE_SIZE - 1);
         pg.block_list_head = NULL;
         pg.next = NULL;
-        memcpy(PHYSICAL_MEMORY, &pg, sizof(pg));//undefined ref to sizeof
-
+        memcpy(PHYSICAL_MEMORY, &pg, sizeof(pg));//undefined ref to sizeof
         PAGE_LIST.head = PHYSICAL_MEMORY;
 }
 
+/* Given a page and a size, allocates an appropriate block of memory within that page. */
+static void *allocate_block(int size, struct page *current_page) {
+
+
+
+        struct block *tmp_curr = current_page->block_list_head;
+        struct block *tmp_prev;
+        while (tmp_curr != NULL) {
+                /* Find a suitable block and allocate it. */
+                if (tmp_curr->state == UNALLOCATED && tmp_curr->size >= size) {
+                        /*
+                        * Split block into block of given size and size of rest.
+                        * Set the addresses that the the two blocks point to.
+                        * Store the two new blocks in metadata region of memory.
+                        * Adjust the page's block list to account for the two new blocks.
+                        */
+                        struct block alloc_blk;
+                        alloc_blk.address = tmp_curr.address;
+                        alloc_blk.size = size;
+                        alloc_blk.state = ALLOCATED;
+                        current_page.size_of_allocated += size;
+                        struct block unalloc_blk;
+                        unalloc_blk.address = tmp_curr.address + size;
+                        /* Size of unalloc_blk is based on whether it is the last block in the list or not. */
+                        unalloc_blk.size = tmp_curr.next == NULL ? PAGE_SIZE - current_page.size_of_allocated : tmp_curr.size - alloc_blk.size;
+                        unalloc_blk.state = UNALLOCATED;
+                        /* Adjust the page list accordingly. */
+                        tmp_prev.next = alloc_blk;
+                        alloc_blk.next = unallock_blk;
+                        unalloc_blk.next = tmp_curr.next == NULL ? NULL : tmp_curr.next;
+                        /*
+                        * Function to store block metadata goes here.
+                        * store_block_metadata(size, current_page);
+                        */
+                        //done so should ret address here
+                        break;
+                }
+                tmp_prev = tmp_curr;
+                tmp_curr = tmp_curr->next;
+        }
+
+        return NULL;
+}
+
+/* Allocates memory for the scheduler. */
+static void allocate_for_scheduler(int size) {
+        struct page *tmp = PAGE_LIST.head;
+        my_pthread_t curr_tid = get_current_tid();
+        while (tmp != NULL) { /* Locate correct page for scheduler. */
+                if (tmp->tid == NULL) { /* tid is NULL for scheduler pages. */
+                        /* Is the page full? */
+                        if (tmp->size_of_allocated >= PAGE_SIZE) {
+                                tmp = tmp->next;
+                                continue;
+                        }
+                        allocate_block(size, tmp);
+                }
+                tmp = tmp->next;
+        }
+}
+
+/* Allocates memory for a thread. */
+static void allocate_for_thread(int size) {
+		// need more than 1 page - assuming page struct goes at the top of each page
+		if(size > PAGE_SIZE -sizeof(struct page) ){
+			int num_pages = size/ (PAGE_SIZE - sizeof(struct page));
+
+		}
+	// if size is greater than page need to make more pages
+        struct page *tmp = PAGE_LIST.head;
+        my_pthread_t curr_tid = get_current_tid();
+        while (tmp != NULL) { /* Locate page with matching tid. */
+                if (tmp->tid == curr_tid) { /* This is the threads page. */
+                        /* Is the page full? */
+                        if (tmp->size_of_allocated + size > PAGE_SIZE) {
+                                tmp = tmp->next;
+                                continue;
+                        }
+                        allocate_block(size, tmp);
+                }
+                tmp = tmp->next;
+        }
+}
 
 /*
 * Returns a void pointer to allocated memory.
@@ -40,19 +123,30 @@ static void init_memory_metadata(int size, char *FILE, int *LINE, REQID threadre
 * If there is not enough memory in the page to allocate from, return a NULL pointer.
 * Should communicate with scheduler to know which thread made the request so it knows which page to allocate from.
 */
-void *my_allocate(int size, char *FILE, int *LINE, REQID threadreq) {
+void *my_allocate(int size, char *FILE, int *LINE, REQUEST_ID request_id) {
         if (FIRST_ALLOCATE == 1) {
-              // init_memory_metadata(); - argument passing
-                FIRST_ALLOCATE = 0;
-        }
-        struct page *tmp = PAGE_LIST.head;
-        my_pthread_t curr_tid = get_current_tid();
-        while (tmp != NULL) { /* Locate page with matching tid. */
-                if (tmp->tid == curr_tid) { /* This is the threads page */
-                        /* Call allocate_for_thread(...) here. */
-                }
-        }
 
+
+        	 struct sigaction sa;
+        	        sa.sa_flags = SA_SIGINFO;
+        	        sigemptyset(&sa.sa_mask);
+        	        sa.sa_sigaction = signal_handler;
+
+        	        if (sigaction(SIGSEGV, &sa, NULL) == -1)
+        	        {
+        	            printf("Fatal error setting up signal handler\n");
+        	            exit(EXIT_FAILURE);    //explode!
+        	        }
+
+                init_memory_metadata(request_id);
+                FIRST_ALLOCATE = 0;
+        } else if (request_id == LIBRARYREQ) {
+                allocate_for_scheduler(size);
+        } else if (request_id == THREADREQ) {
+                allocate_for_thread(size);
+        } else {
+                exit(-1); /* Unknown value for request_id given. */
+        }
         return NULL;
 }
 
@@ -61,12 +155,55 @@ void *my_allocate(int size, char *FILE, int *LINE, REQID threadreq) {
 * Finds the page associated with the calling thread and frees allocated memory from it.
 * Should communicate with scheduler to know which thread made the request so it knows which page to free the allocation from.
 */
-void my_deallocate(void *ptr, char *FILE, int *LINE, REQID threadreq) {
+void my_deallocate(void *ptr, char *FILE, int *LINE, REQUEST_ID request_id) {
+
+
+
+	struct block free = (struct block)((char *)ptr - sizeof(struct block)); // should point to block struct
+
+	if(free->state == UNALLOCATED){
+		printf("tried to free unallocated space\n");
+	}
+
+	free->state = UNALLOCATED;
+
+		/*
+		 * check to see if next is free then merges - do not need while loop will check every free
+		 */
+	if(free->next != NULL && free->next->state == UNALLOCATED) {
+			struct block tmp = free->next;
+			if(free->next != NULL) {
+				free->next = free->next->next;
+			}
+
+			free->size += sizeof(struct block) + tmp->size;
+
+
+		}
+
+	/*
+	 * We need a way to combine blocks before it or we will slowly lose memory -added a prev to block struct
+	 */
+
+	if(free->prev != NULL && free->prev->state == UNALLOCATED) {
+			if(free->next != NULL) {
+				free->next->prev = free->prev;
+			}
+			if(free->prev != NULL) {
+				free->prev->next = free->next;
+			}
+			free->prev->size += sizeof(struct block) + free->size;
+
+
+		}
+
+
+
+}
+
+void signal_handler(int signal, siginfo_t *si, void *unused) {
 
 }
 
 
-int main(int argc, char **argv){
-printf("test\n");
-return 1;
-}
+
--- memory_manager.h
+++ memory_manager.h
@@ -1,16 +1,27 @@
 #include <stdlib.h>
 #include "my_pthread_t.h"
 
+enum REQUEST_ID {
+        LIBRARYREQ,
+        THREADREQ
+};
 
+enum BLOCK_STATE {
+        UNALLOCATED,
+        ALLOCATED
+};
 
 struct block {
-        size_t size;
-        char *data;
+        enum BLOCK_STATE state;
+        int size;
+        char *address;
         struct block *next;
+        struct block *prev;
 };
 
 struct page {
         my_pthread_t tid;
+        int size_of_allocated;
         char *start_address;
         char *end_address;
         struct block *block_list_head;
@@ -23,13 +34,12 @@ struct page_list {
 
 
 
-typedef enum {THREADREQ, LIBRARYREQ} REQID;
 // When user calls malloc or free from a thread.
+#define malloc(x) my_allocate(x, __FILE__, __LINE__, REQUEST_ID)
 
-#define malloc(x) my_allocate(x, __FILE__, __LINE__, THREADREQ)
+#define free(x) my_deallocate(x, __FILE__, __LINE__, REQUEST_ID)
 
-#define free(x) my_deallocate(x, __FILE__, __LINE__, THREADREQ)
+void *my_allocate(int size, char *FILE, int *LINE, REQUEST_ID request_id);
 
-void *my_allocate(int size, char *FILE, int *LINE, REQID threadreq);
+void my_deallocate(void *ptr, char *FILE, int *LINE, REQUEST_ID request_id);
 
-void my_deallocate(void *ptr, char *FILE, int *LINE, REQID threadreq);
